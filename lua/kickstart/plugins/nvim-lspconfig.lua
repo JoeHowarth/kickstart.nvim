4bae983354396dc172361a86d2a2ec65e1c7e5ec
Joe Howarth <josephehowarth@gmail.com>
2024-11-04 10:32:13 -0600

    remove delve, use denols instead of tsserve (need better setup for both deno and node...), remove comments, fix colorscheme highlights

--- lua/kickstart/plugins/nvim-lspconfig.lua
+++ lua/kickstart/plugins/nvim-lspconfig.lua
@@ -147,7 +147,8 @@ return { -- LSP Configuration & Plugins
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {},
+      -- ts_ls = {},
+      denols = {},
       jsonls = {},
       --
 
