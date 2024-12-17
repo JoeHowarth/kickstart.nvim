931687ca5cb043914df6f2492920b286c9611cc6
Joe Howarth <joe@monad.xyz>
2024-12-17 14:56:25 -0500

    yank over ssh

--- init.lua
+++ init.lua
@@ -16,6 +16,27 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Support yank to clipboard over ssh
+local is_tmux_session = vim.env.TERM_PROGRAM == 'tmux' -- Tmux is its own clipboard provider which directly works.
+-- TMUX documentation about its clipboard - https://github.com/tmux/tmux/wiki/Clipboard#the-clipboard
+if vim.env.SSH_TTY and not is_tmux_session then
+  local function paste()
+    return { vim.fn.split(vim.fn.getreg '', '\n'), vim.fn.getregtype '' }
+  end
+  local osc52 = require 'vim.ui.clipboard.osc52'
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = osc52.copy '+',
+      ['*'] = osc52.copy '*',
+    },
+    paste = {
+      ['+'] = paste,
+      ['*'] = paste,
+    },
+  }
+end
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
